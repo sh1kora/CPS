e0d3ec9392e9d58766e533495571077e4c2b1114
Shi Kora <124691481+sh1kora@users.noreply.github.com>
2025-01-07 23:50:49 +0400

    1.3 cleaning comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Универсальная функция для управления активным классом
+// Универсальная функция для управления активными классами меню
 function handleActiveClass(elements, activeClass) {
     elements.forEach(function (item) {
         item.addEventListener('click', function () {
@@ -10,55 +10,20 @@ function handleActiveClass(elements, activeClass) {
     });
 }
 
-// Применение универсальной функции
+
 handleActiveClass(document.querySelectorAll('.menu__item'), 'menu__item--active');
 handleActiveClass(document.querySelectorAll('.sidebar__item'), 'sidebar__item--highlighted');
 handleActiveClass(document.querySelectorAll('.sidebar__lang'), 'sidebar__lang--active');
 
-const openBtn = document.querySelector('.burger-menu'); // Используем querySelector для одного элемента
+// Управление сайдбаром
+const openBtn = document.querySelector('.burger-menu');
 const closeBtn = document.querySelector('.burger-close');
-const sidebar = document.querySelector('.sidebar'); // querySelector для поиска первого совпадения
+const sidebar = document.querySelector('.sidebar');
 
 openBtn.addEventListener('click', () => {
-    sidebar.classList.remove('sidebar--hidden'); // Убираем скрытие
+    sidebar.classList.remove('sidebar--hidden');
 });
 
 closeBtn.addEventListener('click', () => {
-    sidebar.classList.add('sidebar--hidden'); // Добавляем скрытие
+    sidebar.classList.add('sidebar--hidden');
 });
-
-
-// // выбор активного пункта в меню в <main>
-// const contentMenuItem = document.querySelectorAll('.menu__item')
-//
-// contentMenuItem.forEach(function (item) {
-//     item.addEventListener('click', function () {
-//         contentMenuItem.forEach(function (el){
-//             el.classList.remove('menu__item--active')
-//         })
-//         item.classList.add('menu__item--active')
-//     })
-// })
-//
-// const sidebarMenuItem = document.querySelectorAll('.sidebar__item')
-//
-// sidebarMenuItem.forEach(function (item) {
-//     item.addEventListener('click', function () {
-//         sidebarMenuItem.forEach(function (el){
-//             el.classList.remove('sidebar__item--highlighted')
-//         })
-//         item.classList.add('sidebar__item--highlighted')
-//     })
-// })
-//
-// const sidebarLang = document.querySelectorAll('.sidebar__lang')
-//
-// sidebarLang.forEach(function (item) {
-//     item.addEventListener('click', function () {
-//         sidebarLang.forEach(function (el){
-//             el.classList.remove('sidebar__lang--active')
-//         })
-//         item.classList.add('sidebar__lang--active')
-//     })
-// })
-
